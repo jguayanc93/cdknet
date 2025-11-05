f0b3c77c571b6e3272691efe715eeb8c361dcd06
juan carlos <jguayancastillo21@gmail.com>
2025-11-05 14:21:01 -0500

    configurado correctamente el cors

--- cors/conf.js
+++ cors/conf.js
@@ -1,5 +1,11 @@
-const corhabilitaciones={
+const corhabilitaciones2={
     origin:"http://127.0.0.1",
+    methods:['GET','POST'],
+    credentials:true
+}
+
+const corhabilitaciones={
+    origin:"https://landing.compudiskett.com.pe",
     methods:['GET','POST'],
     credentials:true
 }
