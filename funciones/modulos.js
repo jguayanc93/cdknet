3ac91c3b85065f229b84abbf56bc64117f8a0210
juan carlos <jguayancastillo21@gmail.com>
2025-11-11 11:17:07 -0500

    agregado una mejor combinacion del objeto modulos

--- funciones/modulos.js
+++ funciones/modulos.js
@@ -1,4 +1,4 @@
-
+/////esto se usara mas adelante para dar ciertos permisos por usuario
 const cotizacion=["crear","modificar","eliminar"];
 // const pedidos=["crear","modificar","eliminar"];
 const facturas=["modificar","despachar"];
@@ -9,23 +9,21 @@ const cuota=["volumen","rentabilidad","cobertura"];
 
 // const modulos_generales=[cotizacion,facturas,promocion,reporte];
 
-
-// const grp20=[modulos_generales[0],"","","",""];
 const grp20={
-    "cotizacion":[cotizacion[0],cotizacion[1]],
-    "listas":[listas[0],listas[1],listas[2]],
-    "facturas":[facturas[0],facturas[1]],
-    "promocion":[promocion[0],promocion[1]],
-    "cuota":[cuota[0]]
+    "cotizacion":"Permite manejar cotizaciones",
+    "listas":"Lista tus ventas",
+    "factura":"Permite manejar facturas",
+    "promocion":"Permite adjuntar y retirar promociones",
+    "cuota":"Ver el avance de las cuotas"
 }
 const grp25={
-    "cotizacion":[cotizacion[0],cotizacion[1],cotizacion[2]],
-    "promocion":[promocion[0],promocion[1]],
-    "reporte":[reporte[0]]
+    "cotizacion":"Permite manejar cotizaciones",
+    "promocion":"Permite adjuntar y retirar promociones",
+    "reporte":"Permite sacar reportes de marcas"
 };
 const grp34={
-    "facturas":[facturas[0],facturas[1]],
-    "promocion":[promocion[0],promocion[1]]
+    "factura":"Permite manejar facturas",
+    "promocion":"Permite adjuntar y retirar promociones"
 };
 // const grp39=[];
 
