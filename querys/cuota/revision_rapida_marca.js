3ad458dfb4c44b7136341fdf98c183b51eea52ef
juan carlos <jguayancastillo21@gmail.com>
2025-12-03 19:37:45 -0500

    agregado la revision fast para ver si la marca ya tiene una cuota

---
+++ querys/cuota/revision_rapida_marca.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const {Request,TYPES} = require('../../conexion/cadena')
+
+let revision_rapida_marca_existe = (resolve,reject,conexion,galleta,body)=>{
+
+    let codigo=body.codigo;
+
+    // let sq_sql="select * from tbl01api_jefaturas_meta where anno=YEAR(GETDATE()) AND mes=MONTH(GETDATE()) AND codmar=@marc AND codusu=@codusu";
+    let sq_sql="select * from tbl01api_jefaturas_meta where anno=YEAR(GETDATE()) AND mes=MONTH(GETDATE()) AND codmar=@codmar";
+    let consulta= new Request(sq_sql,(err,rowCount,rows)=>{
+        if(err){
+            conexion.close();
+            reject("error query");
+        }
+        else{
+            conexion.close();
+                        
+            if(rows.length==0){
+                resolve("puede registrar cuota de esta marca");
+            }
+            else{
+                reject("tiene un registro marca");
+            }
+        }
+    })
+    consulta.addParameter('codmar',TYPES.VarChar,codigo);
+    conexion.execSql(consulta);
+}
+
+module.exports={revision_rapida_marca_existe}
