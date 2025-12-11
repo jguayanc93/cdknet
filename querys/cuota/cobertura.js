f6e0e1c8816a62263b3cd6e46e29bca54fc83ca0
juan carlos <jguayancastillo21@gmail.com>
2025-12-11 11:50:50 -0500

    corregido el valor de la suma de cobertura

--- querys/cuota/cobertura.js
+++ querys/cuota/cobertura.js
@@ -3,7 +3,7 @@ const {Request,TYPES} = require('../../conexion/cadena')
 
 let cuota_avance_cobertura = (resolve,reject,conexion,galleta,diferenciador)=>{
 
-    let sq_sql="select SUM(tota) from mst01fac where YEAR(fecha)=YEAR(GETDATE()) AND MONTH(fecha)=MONTH(GETDATE()) AND flag<>'*' AND codvta<>'04' AND codven_usu=@codven";
+    let sq_sql="select SUM(CASE mone WHEN 'D' THEN tota WHEN 'S' THEN tota/tcam END) from mst01fac where YEAR(fecha)=YEAR(GETDATE()) AND MONTH(fecha)=MONTH(GETDATE()) AND flag<>'*' AND codvta<>'04' AND codven_usu=@codven";
     let consulta= new Request(sq_sql,(err,rowCount,rows)=>{
         if(err){
             conexion.close();
